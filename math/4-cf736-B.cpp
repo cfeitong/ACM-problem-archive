c35e18a1c24b46661e42c0436bcf736e8cb30349
陈斐童 <cfeitong@gmail.com>
2016-11-28 20:28:22 +0800

    cf 736

---
+++ math/4-cf736-B.cpp
@@ -0,0 +1,127 @@
+
+#include <functional>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iostream>
+#include <bitset>
+#include <cctype>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <string>
+#include <map>
+#include <stack>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <sstream>
+#include <queue>
+#include <climits>
+#include <complex>
+using namespace std;
+
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define INF 0x3f3f3f3f
+#define F first
+#define S second
+#define speedup ios_base::sync_with_stdio(0);cin.tie(0)
+#define all(c) c.begin(), c.end()
+#define rep(i, j, k) for (int i = (j); i < (int)(k); i++)
+#define rep2(i,j, f1,t1, f2,t2) for (int i = (f1); i < (int)(t1); i++) for (int j = (f2); j < (int)(t2); j++)
+#define Rep(i, j, k) for (int i = (j); i <= (int)(k); i++)
+#define Rep2(i,j, f1,t1, f2,t2) for (int i = (f1); i <= (int)(t1); i++) for (int j = (f2); j <= (int)(t2); j++)
+#define per(i, j, k) for (int i = (j); i >= (int)(k); i--)
+typedef long long             LL;
+typedef long double           LD;
+typedef pair<LL, LL>          PLL;
+typedef pair<int, int>        PII;
+typedef pair<LD, LD>          PDD;
+typedef vector<LD>            VD;
+typedef vector<PDD>           VDD;
+typedef vector< vector<PDD> > VVD;
+typedef vector<LL>            VL;
+typedef vector<PLL>           VLL;
+typedef vector< vector<PLL> > VVL;
+typedef vector<int>           VI;
+typedef vector<PII>           VII;
+typedef vector<vector<PII> >  VVI;
+
+bool in(LD       &v) {return scanf("%Lf",  &v)!=EOF;}
+bool in(LL       &v) {return scanf("%lld", &v)!=EOF;}
+bool in(int      &v) {return scanf("%d",   &v)!=EOF;}
+bool in(char     *v) {return scanf("%s",    v)!=EOF;}
+bool in(char     &v) {return scanf("%c",   &v)!=EOF;}
+bool in(unsigned &v) {return scanf("%u",   &v)!=EOF;}
+bool in(string   &v) {return (bool)(cin >> v);}
+template<typename T, typename... Args> bool in(T &v, Args &...args) {bool ret = in(v); return ret && in(args...);}
+
+void out(LD          v) {printf("%Lf",  v);}
+void out(LL          v) {printf("%lld", v);}
+void out(int         v) {printf("%d",   v);}
+void out(char        v) {printf("%c",    v);}
+void out(unsigned    v) {printf("%u",   v);}
+void out(string      v) {printf("%s",    v.c_str());}
+void out(const char *v) {printf("%s",    v);}
+template<typename T> void out(vector<T> &v) {for (int i = 0; i < (int)v.size(); i++) out(v[i]);}
+template<typename T, typename ...Args> void out(T v, Args ...args) {out(v); out(args...);}
+
+LL prime[6] = {2, 3, 5, 233, 331};
+LL qmul(LL x, LL y, LL mod) { // 乘法防止溢出， 如果p * p不爆LL的话可以直接乘； O(1)乘法或者转化成二进制加法
+
+
+    return (x * y - (long long)(x / (long double)mod * y + 1e-3) *mod + mod) % mod;
+    /*
+      LL ret = 0;
+      while(y) {
+      if(y & 1)
+      ret = (ret + x) % mod;
+      x = x * 2 % mod;
+      y >>= 1;
+      }
+      return ret;
+    */
+}
+LL qpow(LL a, LL n, LL mod) {
+    LL ret = 1;
+    while(n) {
+        if(n & 1) ret = qmul(ret, a, mod);
+        a = qmul(a, a, mod);
+        n >>= 1;
+    }
+    return ret;
+}
+
+bool Miller_Rabin(LL p) {
+    if(p < 2) return 0;
+    if(p != 2 && p % 2 == 0) return 0;
+    LL s = p - 1;
+    while(! (s & 1)) s >>= 1;
+    for(int i = 0; i < 5; ++i) {
+        if(p == prime[i]) return 1;
+        LL t = s, m = qpow(prime[i], s, p);
+        while(t != p - 1 && m != 1 && m != p - 1) {
+            m = qmul(m, m, p);
+            t <<= 1;
+        }
+        if(m != p - 1 && !(t & 1)) return 0;
+    }
+    return 1;
+}
+
+int main() {
+
+    LL t; cin >> t;
+    if (t <= 3) cout << 1 << endl;
+    else if (t <= 4) cout << 2 << endl;
+    else if (Miller_Rabin(t)) cout << 1 << endl;
+    else if (Miller_Rabin(t-2)) cout << 2 << endl;
+    else cout << (t&1 ? 3 : 2) << endl;
+
+    return 0;
+}
